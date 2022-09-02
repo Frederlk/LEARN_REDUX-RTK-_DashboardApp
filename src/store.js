dd37250fa8a64142de76b633afa63f80b33105d4
Sergey Petrinich <petrinichshadow@gmail.com>
2022-09-02 14:44:57 +0300

    added persistor

--- src/store.js
+++ src/store.js
@@ -1,25 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore /* persistReducer */ } from "redux-persist";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults
+
 import { filterReducer } from "./features/filter/filterSlice";
 import { positionReducer } from "./features/positions/postionSlice";
 
-// const persistConfig = {
-//     key: "root",
-//     storage,
-//     // обычно используется один лист
-//     whitelist: ["filters", "positions"], // сохраняет определенные редюсеры
-//     // blacklist:[] // удаляет определенные редюсеры
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export const store = configureStore({
-    reducer: {
-        filters: filterReducer,
-        positions: positionReducer,
-    },
-    devTools: true,
+const rootReducer = combineReducers({
+    filters: filterReducer,
+    positions: positionReducer,
 });
 
-// export const persistor = persistStore(store);
+const persistConfig = {
+    key: "root",
+    storage,
+    // обычно используется один лист
+    whitelist: ["filters", "positions"], // сохраняет определенные редюсеры
+    // blacklist:[] // удаляет определенные редюсеры
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    devTools: true,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+export const persistor = persistStore(store);
